6c114a6f95060bb5eab6206d797d550cc7b1f45d
Mailf <mailf.channel@gmail.com>
2025-09-03 15:35:17 +0300

    Update Celery beat schedule to run every 30 minutes using crontab

--- app/celery.py
+++ app/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
@@ -19,6 +20,6 @@ app.autodiscover_tasks()
 app.conf.beat_schedule = {
     'check-payments': {
         'task': 'app.payments.tasks.check_payments',
-        'schedule': 60.0,
+        'schedule': crontab(minute="*/30"),
     },
 }
